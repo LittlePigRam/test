bf89b2546462030a1263537e137f3fae7b1d4a8a
LittlePigRam <aiousi>
2014-06-04 12:17:07 +0800

    2
    2

--- ch2.2.1/ch2.2.1/FirstViewController.m
+++ ch2.2.1/ch2.2.1/FirstViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"test github");
+    NSLog(@"test github sadfsadfsad");
 }
 
 - (void)didReceiveMemoryWarning
